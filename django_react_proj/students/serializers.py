ad7d4171cb5e140e11de2869812ad4b915e7258b
Duc Le <ducle147@gmail.com>
2020-07-25 19:52:30 -0400

    Initialize Serialization and setup urls

---
+++ django_react_proj/students/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import students
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate')
+    # The Meta class is important here because it defines the metadata information
+    # that our model has (database) and that must be converted to the Student class.
+
+    
